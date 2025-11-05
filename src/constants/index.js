d67e5af1b0dc906f2381c31894b29c39ab90904c
kumaran2048 <kumarans2048.sse@saveetha.com>
2025-11-05 19:29:47 +0530

    Resolved merge conflicts

---
+++ src/constants/index.js
@@ -0,0 +1,241 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Excel Analytics Platform",
+description:
+  "An interactive web application for visualizing Excel data, enabling users to upload files, map data dynamically, and generate 2D/3D charts for analysis.",
+subDescription: [
+  "Developed a full-stack MERN application with secure JWT-based user and admin authentication.",
+  "Integrated Excel file parsing using SheetJS and dynamic chart generation using Chart.js and Three.js.",
+  "Built user and admin dashboards for managing uploaded datasets, analysis history, and activity logs.",
+  "Implemented state management with Redux Toolkit, backend file handling with Multer, and real-time analytics using MongoDB aggregation.",
+],
+    href: "https://excellab1.netlify.app/",
+    logo: "",
+    image: "/assets/projects/excel.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 3,
+        name: "Node & Express",
+        path: "/assets/logos/node.png",
+      },
+      {
+        id: 4,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Metaverse Gaming Platform",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Developed a responsive restaurant booking website using HTML, CSS, and JavaScript.",
+  "Designed a clean and intuitive UI allowing users to navigate the menu and book tables easily.",
+  "Implemented a reservation form with input validation and interactive feedback.",
+  "Optimized layout and interactions for seamless usability across devices.",
+  "Planned backend integration for dynamic table booking and database-driven reservation storage.",
+    ],
+    href: "https://metaverse-gaming-platform.vercel.app/", 
+    logo: "",
+    image: "/assets/projects/meta.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      
+    ],
+  },
+  {
+    id: 3,
+    title: "Restaurant Booking Website",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Developed a responsive restaurant booking website using HTML, CSS, and JavaScript.",
+  "Designed a clean and intuitive UI allowing users to navigate the menu and book tables easily.",
+  "Implemented a reservation form with input validation and interactive feedback.",
+  "Optimized layout and interactions for seamless usability across devices.",
+  "Planned backend integration for dynamic table booking and database-driven reservation storage.",
+    ],
+    href: "https://restaurant-booking-system-omega.vercel.app/", 
+    logo: "",
+    image: "/assets/projects/restaurant.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "E-commerce Platform",
+    description:
+      "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
+    subDescription: [
+      "Developed a fully interactive Single Page Application (SPA) using Blazor WebAssembly.",
+      "Implemented API interactions using .NET Core for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated SQLite for efficient client-side database storage.",
+    ],
+    href: "https://e-commerce-web-virid.vercel.app/",
+    logo: "",
+    image: "/assets/projects/omnera.png",
+       tags: [
+      {
+        id: 1,
+        name: "HTML",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Github",
+    href: "https://github.com/Kumaran2048",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/kumaran-s-6823b2338/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name:"Leetcode",
+    href:"https://leetcode.com/u/KUMARAN23/",
+    icon:"/assets/socials/leetcode.svg",
+  }
+];
+
+export const experiences = [
+  {
+  title: "Web Developer Intern",
+  job: "Zidio Development",
+  date: "July 2025 - Sept 2025",
+  contents: [
+    "Developed a full-stack Excel Analytics Platform using the MERN stack (MongoDB, Express.js, React.js, Node.js) with features like file upload, Excel parsing (SheetJS), and 2D/3D chart visualization (Chart.js, Three.js).",
+    "Implemented user & admin authentication (JWT), dashboards with upload history, and dynamic data mapping for generating downloadable graphs (PNG/PDF).",
+    "Integrated modern UI with Tailwind CSS, optimized backend API routes, and enabled optional AI-powered insights using OpenAI API for automated data summaries.",
+  ],
+},
+ {
+  title: "Cloud Architect Intern",
+  job: "Network Geeks",
+  date: "Aug 2025 - Sep 2025",
+  contents: [
+    "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+    "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control.",
+    "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+    "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    "💡 Gained hands-on experience with cloud technologies and networking fundamentals.",
+    "☁️ Worked extensively with AWS services including EC2, VPC, IAM, Route 53, and S3 to build and deploy scalable cloud solutions.",
+    "🚀 Strengthened technical proficiency and confidence in cloud architecture and infrastructure management."
+  ],
+},
+{
+  title: "Data Science Intern",
+  job: "Gradtwin",
+  date: "Oct 2025 - Present",
+  contents: [
+    "Engaged in practical learning of core Data Science and Machine Learning concepts.",
+    "📊 Working on Exploratory Data Analysis (EDA), data inspection, data cleaning, and feature engineering techniques.",
+    "🧠 Gaining experience in handling real-world datasets and improving data preprocessing and transformation skills.",
+    "🔍 Learning model-building workflows and developing understanding of statistical methods and performance evaluation.",
+    "🚀 Enhancing analytical thinking and problem-solving abilities through hands-on project work and continuous experimentation.",
+  ],
+},
+
+];
+export const reviews = [
+  {
+    name: "Sabarish",
+    username: "@sabarish",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "karthik",
+    username: "@karthik",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "sachin",
+    username: "@sachin",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "shaurya pandey",
+    username: "@shaurya",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Afzal",
+    username: "@Afzal",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Harsha",
+    username: "@harsha",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Manish",
+    username: "@manish",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Rajesh",
+    username: "@rajesh",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
